1e7b903bd875e9fddecb7bae74c5358e6e0e67eb
Yue Gao <11145354+Lodour@users.noreply.github.com>
2022-02-10 13:35:59 -0600

    feat: improved test script

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,7 @@
 import argparse
 import os
-from typing import Any, Optional
+from functools import partial
+from typing import Optional, Callable
 
 import numpy as np
 import torch.nn as nn
@@ -11,7 +12,7 @@ from torchvision.datasets import CIFAR10
 from tqdm import trange
 
 from src.art.classifier import PyTorchClassifier
-from src.defenses import DEFENSES, EOT, Ensemble
+from src.defenses import DEFENSES, EOT, RandomizedPreprocessor, Ensemble
 from src.models import CIFAR10ResNet
 
 PRETRAINED_MODELS = {
@@ -28,36 +29,100 @@ def parse_args():
     parser.add_argument('--data-dir', type=str, default='static/datasets')
     parser.add_argument('-b', '--batch', type=int, default=1000)
     parser.add_argument('-g', '--gpu', type=int, default=0)
-    parser.add_argument('-n', type=int, default=20)
+    parser.add_argument('-n', type=int, default=10)
+    parser.add_argument('--data-skip', type=int, default=10)
     # attack
     parser.add_argument('--norm', type=str, default='inf')
     parser.add_argument('--eps', type=float, default=8)
     parser.add_argument('--lr', type=float, default=2)
     parser.add_argument('--step', type=int, default=10)
-    parser.add_argument('-a', '--adaptive', action='store_true')
     # defense
-    parser.add_argument('-d', '--defenses', type=str, choices=DEFENSES, nargs='+', required=True)
+    parser.add_argument('-d', '--defenses', type=str, choices=DEFENSES, nargs='+')
     parser.add_argument('-k', type=int, default=2)
-    parser.add_argument('-r', '--randomized', action='store_true')
     parser.add_argument('--eot', type=int, default=1)
     args = parser.parse_args()
     return args
 
 
-def get_wrapper(model: nn.Module, defense: Optional[PreprocessorPyTorch] = None):
-    wrapper = PyTorchClassifier(
-        model, loss=nn.CrossEntropyLoss(), input_shape=(3, 32, 32), nb_classes=10, clip_values=(0, 1),
-        preprocessing=None, preprocessing_defences=defense,
+def load_defense(args):
+    """Single defense"""
+    # defense = DEFENSES['Gaussian'].as_randomized()
+
+    """Randomized ensemble of all"""
+    defense = Ensemble(
+        randomized=True,
+        preprocessors=[DEFENSES[p].as_randomized() for p in args.defenses],
+        k=args.k,
     )
-    return wrapper
+
+    """Manually specified ensemble"""
+    # defense = Ensemble(
+    #     randomized=False,
+    #     preprocessors=[
+    #         DEFENSES['Gaussian'].as_fixed(kernel_size=(5, 5), sigma=(1, 1)),
+    #         # DEFENSES['Median'].as_fixed(kernel_size=(5, 5)),
+    #         # DEFENSES['JpegCompression'].as_fixed(quality=60),
+    #     ],
+    #     k=3,
+    # )
+    return defense
 
 
-def robust_predict(model: PyTorchClassifier, x_test: np.ndarray, y_test: np.ndarray, args: Any):
-    correct = np.ones_like(y_test)
-    for _ in trange(args.n, desc='Predict'):
-        preds = model.predict(x_test, batch_size=args.batch).argmax(1)
-        correct &= preds == y_test
-    return correct
+class TestKit(object):
+
+    def __init__(
+        self,
+        model: nn.Module,
+        defense: RandomizedPreprocessor,
+        attack_fn: Callable,
+        batch_size: int,
+        repeat: int,
+    ):
+        self.model = model
+        self.defense = defense
+        self.model_defended = self.get_wrapper(model, defense=defense)
+        self.attack_fn = attack_fn
+        self.batch_size = batch_size
+        self.repeat = repeat
+
+    def predict(self, x_test: np.ndarray, y_test: np.ndarray):
+        # Initialize slots for prediction's correctness
+        correct = np.zeros_like(y_test)
+
+        # Repeat prediction and logic OR the results
+        # this means, the final prediction is correct as long as it corrects once.
+        for _ in trange(self.repeat, desc='Union Predict', leave=False):
+            preds = self.model_defended.predict(x_test, batch_size=self.batch_size).argmax(1)
+            correct |= preds == y_test
+
+        return correct, np.mean(correct) * 100
+
+    def attack(self, x_test: np.ndarray, y_test: np.ndarray, adaptive: bool, eot_samples: int = 1):
+        # Do we attack defended or undefended model?
+        defense = EOT(self.defense, nb_samples=eot_samples) if adaptive else None
+        target_model = self.get_wrapper(self.model, defense=defense)
+
+        # Adjust batch size according to EOT samples.
+        batch_size = max(1, self.batch_size // eot_samples)
+
+        # Final attack
+        attack = self.attack_fn(target_model, batch_size=batch_size)
+        x_adv = attack.generate(x_test, y_test)
+
+        return self.predict(x_adv, y_test)
+
+    @staticmethod
+    def get_wrapper(model: nn.Module, defense: Optional[PreprocessorPyTorch] = None):
+        wrapper = PyTorchClassifier(
+            model,
+            loss=nn.CrossEntropyLoss(),
+            input_shape=(3, 32, 32),
+            nb_classes=10,
+            clip_values=(0, 1),
+            preprocessing=None,
+            preprocessing_defences=defense,
+        )
+        return wrapper
 
 
 def main(args):
@@ -70,70 +135,45 @@ def main(args):
     else:
         args.norm = int(args.norm)
 
-    # Load test data
+    # Load model
+    model = CIFAR10ResNet.load_from_checkpoint(args.load)
+    logger.debug(f'Loaded model from "{args.load}".')
+
+    # Load data
     dataset = CIFAR10(args.data_dir, train=False)
     x_test = np.array(dataset.data / 255, dtype=np.float32).transpose((0, 3, 1, 2))  # to channel first
     y_test = np.array(dataset.targets, dtype=np.int)
-    x_test = x_test[::10]
-    y_test = y_test[::10]
+    x_test = x_test[::args.data_skip]
+    y_test = y_test[::args.data_skip]
     logger.debug(f'Loaded dataset x: {x_test.shape}, y: {y_test.shape}.')
-
-    # Load defense
-    """Single defense
-    """
-    # defense = DEFENSES['Gaussian'].as_randomized()
-    """Randomized ensemble of all
-    """
-    defense = Ensemble(
-        randomized=args.randomized,
-        preprocessors=[DEFENSES[p].as_randomized() for p in args.defenses],
-        k=args.k,
-    )
-    """Manually specified ensemble 
-    """
-    # defense = Ensemble(
-    #     randomized=False,
-    #     preprocessors=[
-    #         DEFENSES['Gaussian'].as_fixed(kernel_size=(5, 5), sigma=(1, 1)),
-    #         # DEFENSES['Median'].as_fixed(kernel_size=(5, 5)),
-    #         # DEFENSES['JpegCompression'].as_fixed(quality=60),
-    #     ],
-    #     k=3,
-    # )
-    logger.debug(f'Using defense {defense}.')
-
-    if args.eot > 1:
-        args.batch = max(1, args.batch // args.eot)
-        defense = EOT(defense, nb_samples=args.eot)
-        logger.debug(f'Using EOT with {args.eot} samples.')
-    else:
-        logger.debug(f'No EOT used.')
-
-    # Load model
-    model = CIFAR10ResNet.load_from_checkpoint(args.load)
-    model_predict = get_wrapper(model, defense=defense)
-    model_attack = get_wrapper(model, defense=defense if args.adaptive else None)
-
-    # Test benign
-    preds_clean = robust_predict(model_predict, x_test, y_test, args)
-    acc = np.mean(preds_clean)
-    logger.info(f'Accuracy: {acc:.4f}')
-
-    # Only test valid samples
-    indices = np.nonzero(preds_clean)
-    x_test = x_test[indices]
-    y_test = y_test[indices]
-    logger.debug(f'Selecting {len(x_test)} correctly classified samples.')
 
     # Load attack
     logger.debug(f'Attack: norm {args.norm}, eps {args.eps:.5f}, eps_step {args.lr:.5f}, step {args.step}')
-    attack = PGD(model_attack, args.norm, eps=args.eps, eps_step=args.lr, max_iter=args.step, batch_size=args.batch)
+    attack_fn = partial(PGD, norm=args.norm, eps=args.eps, eps_step=args.lr, max_iter=args.step)
 
-    # Test adversarial
-    x_adv = attack.generate(x_test, y_test)
-    preds_adv = robust_predict(model_predict, x_adv, y_test, args)
-    rob = np.mean(preds_adv)
-    logger.info(f'Robustness: {rob:.4f}')
+    # Load defense
+    defense = load_defense(args)
+    logger.debug(f'Defense: {defense}.')
+
+    # Load test
+    testkit = TestKit(model, defense, attack_fn, args.batch, args.n)
+
+    # 1. Test benign
+    preds_clean, acc = testkit.predict(x_test, y_test)
+    logger.info(f'Accuracy: {acc:.2f}')
+
+    # 2. Select correctly classified samples
+    indices = np.nonzero(preds_clean)
+    x_test = x_test[indices]
+    y_test = y_test[indices]
+
+    # 3. Test adversarial (non-adaptive)
+    preds_adv, rob = testkit.attack(x_test, y_test, adaptive=False, eot_samples=1)
+    logger.info(f'Robustness (non-adaptive): {rob:.2f}')
+
+    # 4. Test adversarial (adaptive)
+    preds_adv, rob = testkit.attack(x_test, y_test, adaptive=True, eot_samples=args.eot)
+    logger.info(f'Robustness (adaptive): {rob:.2f}')
 
 
 if __name__ == '__main__':
