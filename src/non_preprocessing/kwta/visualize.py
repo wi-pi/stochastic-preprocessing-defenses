74a1040c766195fa87b7c84a0e093bdda5bc8981
Yue Gao <11145354+Lodour@users.noreply.github.com>
2022-04-01 15:20:56 -0500

    feat: kwta visualize all

--- src/non_preprocessing/kwta/visualize.py
+++ src/non_preprocessing/kwta/visualize.py
@@ -1,4 +1,5 @@
 import argparse
+from functools import partial
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -13,7 +14,10 @@ def load(lr: float, max_step: int, eot: int, sigma: float = 8.0):
 def load_by_lr(max_step: int, eot: int, lr_list: list[float]):
     for lr in lr_list:
         df = load(lr, max_step, eot)
+        df['Gradient Queries'] = df['PGD Steps'] * eot
         df['LR'] = lr
+        df['EOT'] = eot
+        df['TAG'] = df.apply(lambda x: f'EOT = {int(x.EOT)}, LR = {x.LR}', axis=1).astype('category')
         yield df
 
 
@@ -27,6 +31,21 @@ def plot_no_eot():
     plt.savefig(args.plot_dir / args.output, bbox_inches='tight')
 
 
+def plot_all():
+    concat = partial(pd.concat, ignore_index=True)
+    df1 = concat(load_by_lr(max_step=40000, eot=1, lr_list=[0.1, 0.2, 0.5, 1.0]))
+    df2 = concat(load_by_lr(max_step=20000, eot=100, lr_list=[0.5, 1.0, 2.0, 5.0]))
+    df3 = concat(load_by_lr(max_step=100, eot=1000, lr_list=[0.5, 1.0, 2.0, 5.0]))
+    df = concat([df1, df2, df3])
+
+    for col in ['PGD Steps', 'Gradient Queries']:
+        plt.figure(constrained_layout=True)
+        ax = sns.lineplot(data=df, x=col, y='Accuracy', hue='TAG')
+        ax.set(xscale="log")
+        plt.legend(loc='upper right')
+        plt.savefig(args.plot_dir / f'{args.output}_{col.replace(" ", "_")}.pdf', bbox_inches='tight')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--log-dir', type=Path, default='static/logs/kwta')
@@ -34,4 +53,4 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', type=str, required=True)
     args = parser.parse_args()
 
-    plot_no_eot()
+    plot_all()
