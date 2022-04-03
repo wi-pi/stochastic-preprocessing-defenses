dc97dd2442628e201d747daab6a56c83a454a4b1
Yue Gao <11145354+Lodour@users.noreply.github.com>
2022-04-02 23:11:53 -0500

    feat: add odds

--- src/non_preprocessing/README.md
+++ src/non_preprocessing/README.md
@@ -1,3 +1,20 @@
 # Non-Preprocessing Defenses
 
 Implementations are adapted from https://github.com/wielandbrendel/adaptive_attacks_paper
+
+
+## Special Requirements for Each Defense
+
+### k-Winners-Take-All
+```
+foolbox==3.0.0
+```
+
+### Odds
+```
+conda create -n tf1 python=3.7
+conda activate tf1
+conda install cudatoolkit=10.0
+conda install -c anaconda cudnn
+pip install tensorflow-gpu==1.15 cleverhans==3.1.0 jupyter tqdm sklearn numpy
+```
--- src/non_preprocessing/configs.py
+++ src/non_preprocessing/configs.py
@@ -1,13 +1,21 @@
 """
 This script contains static files for each defense.
 """
+import os
+
 from torchvision.datasets.utils import download_url
 
 MODELS = [
     'https://github.com/wielandbrendel/robustness_workshop/releases/download/v0.0.1/kwta_spresnet18_0.1_cifar_adv.pth',
 ]
 
+ZIP_MODELS = {
+    'natural.zip': 'https://www.dropbox.com/s/cgzd5odqoojvxzk/natural.zip?dl=1',
+}
 
 if __name__ == '__main__':
     for url in MODELS:
         download_url(url, './static/models/')
+
+    for name, url in ZIP_MODELS.items():
+        os.system(f'wget -O "./static/models/{name}" "{url}" && unzip "./static/models/{name}" -d "./static/models/"')
